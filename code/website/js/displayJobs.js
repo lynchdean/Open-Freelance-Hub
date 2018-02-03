b8f3919671865f86902f48be8e3ebe173596c7cd
David Weir <david.weir3@mail.dcu.ie>
2018-02-03 15:35:06 +0000

    added individual job page creation & an id to jobs in the contract

--- code/website/js/displayJobs.js
+++ code/website/js/displayJobs.js
@@ -1,5 +1,4 @@
 // display a list of jobs to the html page
-// well hopefully
 
 // initialising web3 provider, or connecting to established provider
 if (typeof web3 !== 'undefined') {
@@ -11,31 +10,56 @@ if (typeof web3 !== 'undefined') {
 var jobs = [];
 
 // address & abi of JobPost.sol contract
-var contractAddr = '0x1c81bee0a13bce16ebc2009ffb37bb43f46c7c48'
+var contractAddr = '0x7ddd0914f033d604d0f9acf060648132181fc370'
 var contractAbi =
-[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"posterAccounts","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getJobCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getJobs","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"desc","type":"string"},{"name":"pay","type":"uint256"}],"name":"addJob","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+
+[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"posterAccounts","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getJobCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getJobs","outputs":[{"name":"","type":"uint256"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"desc","type":"string"},{"name":"pay","type":"uint256"}],"name":"addJob","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+
 
 // getting an intance of hosted contract
 var contractInstance = web3.eth.contract(contractAbi).at(contractAddr);
 
+// ANGULAR
+
 // angularjs to display all jobs
 var app = angular.module('displayPage', []);
 
+// controller to show all jobs uploaded to the blockchain for homepage
 app.controller('showPages', function($scope){
   $scope.jobs = [];
 
   contractInstance.getJobCount.call(function(err, count){
     for (var i=0; i<count; i++){
       contractInstance.getJobs.call(i,function(err,result){
+        console.log(result);
         $scope.$apply(function(){
           var jobObj = {
-            title: result[0],
-            description: result[1],
-            payment: result[2].toNumber()
+            id: result[0],
+            title: result[1],
+            description: result[2],
+            payment: result[3].toNumber()
           }
           $scope.jobs.push(jobObj);
         })
       });
     }
+  })
+})
+
+// controller to show details of one job in an individual job page
+app.controller('showJob', function($scope){
+  $scope.job;
+  var url = (window.location.href).split("%");
+  var jobId = parseInt(url[1].substring(2));
+
+  contractInstance.getJobs.call(jobId, function(err, result){
+    $scope.$apply(function(){
+      $scope.job = {
+        id: result[0],
+        title: result[1],
+        description: result[2],
+        payment: result[3].toNumber()
+      };
+    })
   })
 })
