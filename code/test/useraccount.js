b4e56fda3e5088fdaeeb2742774f0a7532658bde
David Weir <david.weir3@mail.dcu.ie>
2018-02-05 13:52:24 +0000

    added job lists to account contract

--- code/test/useraccount.js
+++ code/test/useraccount.js
@@ -1,16 +1,31 @@
-var UserAccount = artifacts.require("./UserAccount.sol");
+var UserAccount = artifacts.require("./Accounts.sol");
 
 contract('UserAccount', function(accounts){
   it("Should set up an account", function(){
     return UserAccount.deployed().then(function(instance){
       deployedUsers = instance
-      return deployedUsers.setAccount("Dave", "Weir",{from: accounts[0]})
+      return deployedUsers.setAccount(accounts[0], "Dave", "Weir")
     }).then(function(userTrans){
       return deployedUsers.getAccounts.call()
     }).then(function(accs){
       return deployedUsers.getAccount.call(accs[0]);
     }).then(function(me){
       console.log(me);
+    })
+  })
+
+  it("Should add a job as employer", function(){
+    return UserAccount.deployed().then(function(instance){
+      deployedUsers = instance;
+      return deployedUsers.setAccount(accounts[0], "Dave", "Weir")
+    }).then(function(userTrans){
+      return deployedUsers.addEmployerJob(accounts[0], 1);
+    }).then(function(tx){
+      return deployedUsers.getAccounts.call()
+    }).then(function(accs){
+      return deployedUsers.getAccount.call(accs[0]);
+    }).then(function(me){
+      assert.equal(me[2], 1, "Job ID returned incorrectly");
     })
   })
 })
