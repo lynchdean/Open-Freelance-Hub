ff36eeb66e4ef4fb518ab1a948a1de7645909a36
David Weir <david.weir3@mail.dcu.ie>
2018-02-26 10:24:57 +0000

    fix employer jobs display bug

--- code/website/js/displayAccounts.js
+++ code/website/js/displayAccounts.js
@@ -69,9 +69,9 @@ app.controller('showAccountJobs', function($scope){
               jobPostInstance.getWorker.call(result[0], function(err, worker){
                 jobPostInstance.isComplete.call(result[0], function(err, isCompleted){
                   $scope.$apply(function() {
-                    if (worker == accountAddr && !isCompleted){
+                    if (worker == defaultAcc && !isCompleted){
                       status = "Open";
-                    } else if (worker != accountAddr && !isCompleted) {
+                    } else if (worker != defaultAcc && !isCompleted) {
                       console.log(worker)
                       status = "In Progress";
                     } else {
